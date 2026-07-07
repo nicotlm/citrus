7678f7c69109a071f1055b074d16fcaf8ff285df
nicotlm <192305627+ntoulemonde@users.noreply.github.com>
2026-07-07 15:58:20 +0000

    with parse vente

--- src/api.py
+++ src/api.py
@@ -5,7 +5,7 @@ import json
 class bodacc_api:
     def __init__(self, dataset_id="annonces-commerciales", end_point="records"):
         hook = "https://bodacc-datadila.opendatasoft.com/api/explore/v2.1/catalog/datasets"
-        
+
         if end_point:
             self.url = f"{hook}/{dataset_id}/{end_point}"
         else:
@@ -19,6 +19,21 @@ class bodacc_api:
         response = requests.get(request_url)
         return response
 
-    def get_annonce_content(self, annonce_id):
+    def get_annonce_json(self, annonce_id):
         return json.loads(self.get_annonce(annonce_id).content).get("results")[0]
 
+
+def parse_vente(json_dict):
+    json_dict["listeprecedentproprietaire"] = json.loads(json_dict["listeprecedentproprietaire"])
+    json_dict["listepersonnes"] = json.loads(json_dict["listepersonnes"])
+    return {
+        "sirenCedant": json_dict["listeprecedentproprietaire"]["personne"]["numeroImmatriculation"]["numeroIdentification"].replace(" ", ""),
+        'raisonSocialeCedant': json_dict["listeprecedentproprietaire"]["personne"]["denomination"],
+        "sirenBeneficiaire": json_dict["listepersonnes"]["personne"]["numeroImmatriculation"]["numeroIdentification"].replace(" ", ""),
+        "raisonSocialeBeneficiaire": json_dict["listepersonnes"]["personne"]["denomination"],
+        "source": json_dict["url_complete"]
+    }
+
+# 'montantNet' : en ke, à extraire depuis json.loads(json_dict['listeetablissements'])["etablissement"]["origineFonds"] ('siège et établissement principal acquis par achat au prix stipulé de 155000.00 euros')
+# 'dateEffetComptable' : extraire depuis date de commencement ou commentaires ? défaut dateparution ?
+# 'dateRealisationJuridique' : extraire depuis date de commencement ou commentaires ?
