69f441a334aedd128fc81e6171de1cb5504b8889
nicotlm <192305627+ntoulemonde@users.noreply.github.com>
2026-07-08 09:59:53 +0000

    logging

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,22 @@
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
+
+
+# Logging
+logger = logging.getLogger("citrus")
+logger.setLevel(logging.INFO)
+
+# Create handlers
+file_handler = logging.FileHandler("log/citrus.log")
+console_handler = logging.StreamHandler()
+
+# Set level and format
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+# Add handlers
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
