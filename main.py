1d90565f2abf3541eafa73494ca736cfe40acd41
nicotlm <192305627+ntoulemonde@users.noreply.github.com>
2026-07-08 12:27:15 +0000

    vente

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 
-from src.api import bodacc_api, parse_vente
-# import src.llm_client as llm_client
-
-import polars as pl
 import os
 
 if os.getcwd() != '/home/onyxia/work/citrus':
     os.chdir("citrus")
 
+from src.api import bodacc_api, parse_vente
+from src.llm_extract import extract_amount 
+from src.metrics import calculate_metrics
+import polars as pl
+
 api = bodacc_api()
 
-
+###########################
+# Parsing operation Vente
+###########################
 source = "s3://projet-citrus/data/citrus_bodacc.csv"
 
 df = pl.scan_csv(
@@ -29,5 +32,19 @@ df = df.with_columns(
     pl.col("lien_bodacc").str.replace("https://www.bodacc.fr/annonce/detail-annonce/", "").str.replace_all("/", "").alias("num_bodacc")
 )
 
-print(parse_vente(api.get_annonce_json(df[0, "num_bodacc"])))
+l=[]
+for row in df.iter_rows(named=True):
+    bodac_annonce = api.get_annonce_json(row["num_bodacc"])
+    regex_dict = parse_vente(bodac_annonce)
+    llm_dict = extract_amount(bodac_annonce)
+    full_dict = {"num_bodacc": row["num_bodacc"]} | regex_dict | llm_dict
+    l.append(full_dict)
 
+from_bodacc_df = pl.DataFrame(l)
+
+from_bodacc_df = from_bodacc_df.with_columns((pl.col("montantNet")/1000).round()).cast({"montantNet": pl.Int64})
+
+citrus_apibodacc_df = df.join(from_bodacc_df, on="num_bodacc", suffix="_apibodacc") 
+
+calculate_metrics(citrus_apibodacc_df).mean()
+print(citrus_apibodacc_df)
