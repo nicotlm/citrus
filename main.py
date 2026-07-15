306273634f156722a6d24a1253f81f0e6b30b09e
nicotlm <192305627+ntoulemonde@users.noreply.github.com>
2026-07-15 15:59:35 +0000

    update data file path

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ if os.getcwd() != "/home/onyxia/work/citrus":
     os.chdir("citrus")
 
 from src.api import bodacc_api, parse_vente
-from src.llm_extract import extract_amount 
+from src.llm_extract import extract_amount
 from src.metrics import calculate_metrics, filter_metrics_bad, _clean_string
 import polars as pl
 
@@ -14,7 +14,7 @@ api = bodacc_api()
 ###########################
 # Parsing operation Vente
 ###########################
-source = "s3://projet-citrus/data/citrus_bodacc.csv"
+source = "s3://projet-citrus/data/202607_citrus_bodacc_300ventes.csv"
 
 df = pl.scan_csv(
     source,
@@ -32,22 +32,23 @@ df = df.with_columns(
     pl.col("lien_bodacc").str.replace("https://www.bodacc.fr/annonce/detail-annonce/", "").str.replace_all("/", "").alias("num_bodacc")
 )
 
-l=[]
+res_list = []
 for row in df.iter_rows(named=True):
     bodac_annonce = api.get_annonce_json(row["num_bodacc"])
     regex_dict = parse_vente(bodac_annonce)
     llm_dict = extract_amount(bodac_annonce)
     full_dict = {"num_bodacc": row["num_bodacc"]} | regex_dict | llm_dict
-    l.append(full_dict)
+    res_list.append(full_dict)
 
-from_bodacc_df = pl.DataFrame(l)
+from_bodacc_df = pl.DataFrame(res_list)
 
 from_bodacc_df = from_bodacc_df.with_columns((pl.col("montantNet")/1000).round(mode="half_away_from_zero")).cast({"montantNet": pl.Int64})
 
 from_bodacc_df = _clean_string(from_bodacc_df, cols=["raisonSocialeCedant", "raisonSocialeBeneficiaire"])
-citrus_apibodacc_df = df.join(from_bodacc_df, on="num_bodacc", suffix="_apibodacc") 
+citrus_apibodacc_df = df.join(from_bodacc_df, on="num_bodacc", suffix="_apibodacc")
 print(citrus_apibodacc_df)
 
 res = calculate_metrics(citrus_apibodacc_df)
-res.mean()
-filter_metrics_bad(citrus_apibodacc_df, res, "raisonSocialeCedant")
+print(res.mean())
+res.mean().write_csv("res.csv")
+print(filter_metrics_bad(citrus_apibodacc_df, res, "raisonSocialeCedant"))
