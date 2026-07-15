8984ec1981588be17e1fd51031d06d7ee1fdd0f8
nicotlm <192305627+ntoulemonde@users.noreply.github.com>
2026-07-15 16:09:40 +0000

    with luhn formula

--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,25 @@
 
 def annuaire(siren_siret: str):
     return f"https://annuaire-entreprises.data.gouv.fr/entreprise/{siren_siret}"
+
+
+def luhn_checksum(sirene_number):
+    sirene_number = str(sirene_number).replace(" ", "")
+    # Exception pour La Poste
+    if sirene_number == 356000000:
+        return 0
+    def digits_of(n):
+        return [int(d) for d in n]
+    digits = digits_of(sirene_number)
+    odd_digits = digits[-1::-2]
+    even_digits = digits[-2::-2]
+    checksum = 0
+    checksum += sum(odd_digits)
+    for d in even_digits:
+        checksum += sum(digits_of(d*2))
+    return checksum % 10
+
+
+def is_luhn_valid(sirene_number):
+    return luhn_checksum(sirene_number) == 0
+
