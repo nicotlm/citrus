08ef90faddf8cc6e3584d67462bfe2a7aebab3bb
nicotlm <192305627+ntoulemonde@users.noreply.github.com>
2026-07-16 15:47:43 +0000

    update prompt construction

--- src/llm/extract.py
+++ src/llm/extract.py
@@ -2,32 +2,22 @@ from src.llm.client import ask_json
 from src import logger
 
 
-def _build_prompt_llm(text_a_extraire) -> list[dict]:
+def _build_prompt_llm(text_a_extraire, instructions) -> list[dict]:
 
     system = (
         "Tu es un assistant pour extraire des informations d'un journal d'annonces légales. "
         "Tu reponds UNIQUEMENT avec un objet JSON valide, sans aucun texte ni "
-        "balise Markdown autour."
+        "balise Markdown autour. Par exemple si je te demande une balise 'montantNet': "
+        '{{"montantNet": 330000}}"'
     )
 
-    intro = "A partir du contenu donné ci-après, renvoie un objet JSON avec exactement cette clé :"
-    amount_instr = (
-        '- "montantNet" : extrait le montant en euros uniquement à partir du texte',
-        'ci dessous. Ne met pas de centimes ou de virugles, juste le montant en euros',
-        'Par exemple, pour le texte "Etablissement principal acquis par achat au ',
-        'prix stipulé de 330000 EUR" tu dois retourner {"montantNet": 330000}'
-    )
+    user = f"""A partir du contenu donné ci-après, renvoie un objet JSON avec exactement cette clé :
 
-    user = f"""{intro}
+{instructions}
 
-{amount_instr}
 
-\"\"\"
-{text_a_extraire}
-\"\"\"
-
-Reponds uniquement avec le JSON, par exemple :
-{{"montantNet": 330000}}"""
+**Texte dont il faut extraire l'information**
+{text_a_extraire}"""
 
     llm_prompt = [
         {"role": "system", "content": system},
@@ -35,6 +25,27 @@ Reponds uniquement avec le JSON, par exemple :
     ]
     logger.debug(f"LLM prompt is {llm_prompt}")
     return llm_prompt
+
+
+def _build_prompt_llm_amount(text_a_extraire, instructions_exemple) -> list[dict]:
+
+    instr = (
+        '- "montantNet" : extrait le montant en euros uniquement à partir du texte' +
+        ' ci dessous. Ne met pas de centimes ou de virgules, juste le montant en euros. ' +
+        'Par exemple, pour le texte ' + "'" + str(instructions_exemple) + "'" +
+        ' tu dois retourner {"montantNet": 330000}.'
+    )
+
+    return _build_prompt_llm(text_a_extraire=text_a_extraire, instructions=instr)
+
+
+def _build_prompt_llm_vente_amount(text_a_extraire) -> list[dict]:
+    exemple_vente_amount = "Etablissement principal acquis par achat au prix stipulé de 330000 EUR"
+
+    return _build_prompt_llm_amount(
+        text_a_extraire=text_a_extraire,
+        instructions_exemple=exemple_vente_amount
+    )
 
 
 def extract_amount_vente(json_dict):
@@ -48,5 +59,5 @@ def extract_amount_vente(json_dict):
 
     logger.info(f"Extracting amount from '{originefonds}'")
     return ask_json(
-        _build_prompt_llm(originefonds)
+        _build_prompt_llm_vente_amount(originefonds)
     )
