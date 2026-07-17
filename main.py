c8791ae6b2319a16f80d05f030217d146281f3c9
nicotlm <192305627+ntoulemonde@users.noreply.github.com>
2026-07-17 13:42:44 +0000

    print bad rows as md table

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-from src.modele.metrics import filter_metrics_bad
+from src.modele.metrics import filter_metrics_bad_md
 from src.modele.evaluate import evaluate_vente
 
 ###########################
 # Parsing operation Vente
 ###########################
 source = "s3://projet-citrus/data/202607_citrus_bodacc_300ventes.csv"
-# source = "s3://projet-citrus/data/202607_citrus_bodacc_1800op.csv"
 from_bodacc_df, res, citrus_apibodacc_df = evaluate_vente(source)
 
-different_rows = filter_metrics_bad(citrus_apibodacc_df, res, "raisonSocialeCedant")
-print(different_rows)
+filter_metrics_bad_md(citrus_apibodacc_df, res, "raisonSocialeCedant")
 
-different_rows = filter_metrics_bad(citrus_apibodacc_df, res, "dateEffetComptable")
-print(different_rows)
+filter_metrics_bad_md(citrus_apibodacc_df, res, "dateEffetComptable")
+
+# source = "s3://projet-citrus/data/202607_citrus_bodacc_1800op.csv"
